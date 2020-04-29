b79e91b6159de9679f6a77d43e80474ad3328137
GajenderTyagi <gajender@programmers.io>
2020-04-29 12:18:24 +0530

    bat files

---
+++ download.bat
@@ -0,0 +1,35 @@
+@echo off
+rem **************************************************************
+rem  on command prompt navigate to your save file location
+rem  .\upload.bat ibmi.example.com bob mypassword library saveFileName.savf
+rem **************************************************************
+set TEMPFILE=download.fts
+set HOST=%1
+set USER=%2
+set PASS=%3
+set LIB=%4
+set SAVF=%5
+if "%HOST%"=="" goto usage
+if "%USER%"=="" goto usage
+if "%PASS%"=="" goto usage
+echo %USER%> %TEMPFILE%
+echo %PASS%>> %TEMPFILE%
+echo quote site namefmt 1 >> %TEMPFILE%
+echo cd /QSYS.LIB/QGPL.LIB >> %TEMPFILE%
+echo quote site namefmt 0 >> %TEMPFILE%
+echo cd %LIB%>> %TEMPFILE%
+echo bin>> %TEMPFILE%
+echo get %SAVF% %SAVF% >> %TEMPFILE%
+echo quit >> %TEMPFILE%
+ftp -s:%TEMPFILE% %HOST%
+del %TEMPFILE%
+goto end
+:	
+echo.
+echo USAGE: upload HOST USERID PASSWORD
+echo.
+echo      HOST = FTP host you want to upload to (as400.example.com)
+echo    USERID = UserID to log in with
+echo  PASSWORD = Password to log in with
+echo.
+:end
---
+++ upload.bat
@@ -0,0 +1,37 @@
+@echo off
+rem **************************************************************
+rem  on command prompt navigate to your save file location
+rem  .\upload.bat ibmi.example.com bob mypassword saveFileName.savf
+rem **************************************************************
+set TEMPFILE=restore.fts
+set HOST=%1
+set USER=%2
+set PASS=%3
+set SAVF=%4
+set LIB=restore
+if "%HOST%"=="" goto usage
+if "%USER%"=="" goto usage
+if "%PASS%"=="" goto usage
+echo %USER%> %TEMPFILE%
+echo %PASS%>> %TEMPFILE%
+echo quote site namefmt 1 >> %TEMPFILE%
+echo cd /QSYS.LIB/QGPL.LIB >> %TEMPFILE%
+echo quote site namefmt 0 >> %TEMPFILE%
+echo quote crtl %LIB%>> %TEMPFILE%
+echo cd %LIB%>> %TEMPFILE%
+echo bin>> %TEMPFILE%
+echo quote rcmd CRTSAVF FILE(%LIB%/pixobjsavf) >> %TEMPFILE%
+echo put %SAVF% %SAVF% >> %TEMPFILE%
+echo quit >> %TEMPFILE%
+ftp -s:%TEMPFILE% %HOST%
+del %TEMPFILE%
+goto end
+:	
+echo.
+echo USAGE: upload HOST USERID PASSWORD
+echo.
+echo      HOST = FTP host you want to upload to (as400.example.com)
+echo    USERID = UserID to log in with
+echo  PASSWORD = Password to log in with
+echo.
+:end
